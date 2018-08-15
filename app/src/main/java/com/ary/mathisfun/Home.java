e3fd98746499ec9237b390b984d08d17178bd327
Aryan <aryan.thatte@gmail.com>
2018-08-14 20:32:25 -0400

    Actual Code?

---
+++ app/src/main/java/com/ary/mathisfun/Home.java
@@ -0,0 +1,71 @@
+package com.ary.mathisfun;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Home extends AppCompatActivity {
+
+    EditText scoreTxt;
+    EditText totalTxt;
+    TextView percentageTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionxBar(toolbar);
+
+        scoreTxt = (EditText) findViewById((R.id.scoreTxt));
+        totalTxt = (EditText) findViewById(R.id.totalTxt);
+        percentageTxt = (TextView) findViewById(R.id.percentageTxt);
+
+        Button calcBtn = (Button) findViewById(R.id.calcBtn);
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float score = Float.parseFloat(scoreTxt.getText().toString());
+                float total = Float.parseFloat(totalTxt.getText().toString());
+                float percentage = (score / total) * 100;
+                percentageTxt.setText(Float.toString(percentage));
+
+            }
+        });
+    }
+
+    private void setSupportActionxBar(Toolbar toolbar) {
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
